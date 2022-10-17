b38a910ac768b42e3398da83590141aaa79a27ba
[elio] <[castels@hotmail.it]>
2022-10-17 23:49:46 +0100

    fine esercizi

---
+++ Remove exclamation marks/index.js
@@ -0,0 +1,6 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+function removeExclamationMarks(s) {
+  s = 'hello!'
+  return s.substring(0, s.length - 1)
+}
+console.log(removeExclamationMarks())
