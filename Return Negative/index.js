cabb3b4f58769ef6330c38c090086ed12e52f52c
[elio] <[castels@hotmail.it]>
2022-10-18 09:19:19 +0100

    nuovo eserczio inizio

---
+++ Return Negative/index.js
@@ -0,0 +1,10 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+
+function makeNegative(num) {
+  num = 1
+  return num * -1
+}
+console.log(makeNegative())
